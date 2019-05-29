016f2904cda09482e29268a6fb323e7036b2954b
aurimasb <aurimas.bachanovicius@metasite.net>
2019-05-29 20:32:01 +0300

    Updates

---
+++ precache-manifest.81307d07798d72fc6755027fd13e6ce8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d254787e77deb42b91afeb048c9d5fb",
+    "url": "/auction/index.html"
+  },
+  {
+    "revision": "66d29e4b111b415c1a72",
+    "url": "/auction/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "b6a7c824a72551fe5589",
+    "url": "/auction/static/css/main.8db0b71f.chunk.css"
+  },
+  {
+    "revision": "66d29e4b111b415c1a72",
+    "url": "/auction/static/js/2.0ef9ac0a.chunk.js"
+  },
+  {
+    "revision": "b6a7c824a72551fe5589",
+    "url": "/auction/static/js/main.e843cab2.chunk.js"
+  },
+  {
+    "revision": "b9c2f76054098d6e465b",
+    "url": "/auction/static/js/runtime~main.0154db06.js"
+  }
+]);
