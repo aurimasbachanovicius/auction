8c4d292fcb40683b230d8109d8f9cc984ad7335e
Ruta Sipkauskaite <ruta.sipkauskaite@gmail.com>
2019-05-30 17:46:50 +0300

    deleting bootstrap and installing material-kit as dependency

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Search.css';
 
 const Search = ({ categories }) => (
   <form className="form-inline">
@@ -13,16 +14,25 @@ const Search = ({ categories }) => (
       />
       <select className="form-control rounded-0" id="categories">
         <option selected>Categories</option>
-        {categories.map(category => (<option>{category}</option>))}
+        {categories.map(category => (
+          <option>{category}</option>
+        ))}
       </select>
-      <button type="button" className="btn btn-lg btn-primary rounded-0 border-right">Search</button>
-      <button type="button" className="btn btn-lg btn-primary rounded-0">Filters</button>
+      <button
+        type="button"
+        className="btn btn-lg btn-primary rounded-0 border-right"
+      >
+        Search
+      </button>
+      <button type="button" className="btn btn-lg btn-primary rounded-0">
+        Filters
+      </button>
     </div>
   </form>
 );
 
 Search.propTypes = {
-  categories: PropTypes.arrayOf(PropTypes.string).isRequired,
+  categories: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Search;
