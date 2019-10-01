75bbaaa2573a8db8b644cdeffa3577fd6f5a2b73
aurimasb <aurimas.bachanovicius@metasite.net>
2019-10-02 00:17:30 +0300

    todo reminder

--- src/components/Account/Login.jsx
+++ src/components/Account/Login.jsx
@@ -7,6 +7,7 @@ import Grid from '@material-ui/core/Grid';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+// @todo-aurimas fix this props hell. Composite better.
 const Login = ({ onSubmit, onChange, email, password, loggingIn, submitted }) => (
   <form noValidate onSubmit={onSubmit}>
     <TextField
