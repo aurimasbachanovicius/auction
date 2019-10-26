cd4fe308278671a672174bbfbeef456d00ad6a39
Ruta Sipkauskaite <ruta.sipkauskaite@gmail.com>
2019-10-27 02:07:37 +0300

    removed grid fro item overview, incompatibility issues

--- src/pages/Item.jsx
+++ src/pages/Item.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+// import { Grid } from '@material-ui/core';
 import ImageSlider from 'components/Item/ItemOverview/ImageSlider';
 
 const ItemsPage = () => {
   return (
     <React.Fragment>
-      <Grid container spacing={0} justify="center">
+      <ImageSlider />
+      {/* <Grid container spacing={0} justify="center">
         <Grid item md={4}>
           <ImageSlider />
         </Grid>
         <Grid item md={4}></Grid>
         <Grid item md={4}></Grid>
-      </Grid>
+      </Grid> */}
     </React.Fragment>
   );
 };
